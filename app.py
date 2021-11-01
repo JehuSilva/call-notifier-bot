a2c1a5b2caed825c403e941a89c328dd0cef5e4b
Jehu <ijehu.silva@gmail.com>
2021-11-01 18:32:30 -0500

    telegram utils added

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-
+import os
 import json
-from backend import callpicker
-from backend.callpicker import CallPicker
 import pandas as pd
 
+from backend.callpicker import CallPicker
+from backend.messenger import Messenger
+from static.message import notification
 
 
+import logging
+logging.basicConfig(
+    level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+CHANNEL_NAME = os.environ.get('CHANNEL_NAME')
 
 if __name__ == '__main__':
     '''
     This is the main entry point for the application.
     '''
-    callpicker = CallPicker()
-    calls_row = callpicker.get_calls()
+    register = CallPicker()
+    notifier = Messenger(TELEGRAM_TOKEN, CHANNEL_NAME)
+
+    calls_row = register.get_calls()
     calls_df = pd.DataFrame(calls_row)
     calls_df.to_csv('temp/calls.csv', index=False)
-    
 
-    
-
+    # notifier.send_message(notification.format(
+    #     prety_date='2021', date='2021',
+    #     who_answered='some people', customer='customer'
+    # ))
